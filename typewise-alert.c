97fe9b9a308076ac2f1e921f9b35dddfc2507383
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-02-22 23:21:29 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -3,31 +3,24 @@
 
 void (*alertTarget_FuncPtr[])(BreachType)={sendToController,sendToEmail};
 
-void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC) {
-
+void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC)
+{
   BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC );
 
 alertTarget_FuncPtr[alertTarget](breachType);
 }
 
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+    CoolingTypeLimitsConfig LimitsConfigPtr;
+    LimitsConfigPtr = GetLimitsOfCoolingType(coolingType);
+
+  printf("limits->lowerLimit :%d",LimitsConfigPtr.lowerLimit);
+  return inferBreach(temperatureInC, LimitsConfigPtr.lowerLimit, LimitsConfigPtr.upperLimit);
+}
+
+CoolingTypeLimitsConfig GetLimitsOfCoolingType(CoolingType coolingType )
+{
+    return LimitsConfig[coolingType];
 }
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
